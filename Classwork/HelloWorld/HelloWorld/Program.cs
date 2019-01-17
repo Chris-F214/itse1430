a160b01f447070ee2156c8c935b5cc24ff29efcc
christopher.fie51265 <christopher.fie51265@NEITD-123522>
2019-01-16 21:44:04 -0600

    Original ClassWork File

---
+++ Classwork/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            string name;
+
+            Console.WriteLine("Enter your name: ");
+            name = Console.ReadLine();
+            
+            //Never Do!!!
+            //int a, b;
+
+            //string message = "Hello " + name;
+            string message = "Hello ";
+            message += name;
+            Console.WriteLine(message);
+            //Console.Write("Hello ");
+            //Console.WriteLine(name);
+        }
+    }
+}
