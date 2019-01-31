4dd6bd822a682c4f5ae164d7373444ddb3df4536
Christopher Fields <christopher.fields214@my.tccd.edu>
2019-01-31 13:38:37 -0600

    Lab Progress 1/31/19

--- Labs/PizzaCreator/PizzaCreator/Program.cs
+++ Labs/PizzaCreator/PizzaCreator/Program.cs
@@ -10,6 +10,18 @@ namespace PizzaCreator
     {
         static void Main( string[] args )
         {
+            string selection;
+            Console.WriteLine("Welcome to Papa Ceaser's Pizza");
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("1.) Start new order");
+            Console.WriteLine("2.) Modify order");
+            Console.WriteLine("3.) Display order");
+            Console.WriteLine("4.) Exit");
+            Console.WriteLine("Please make a selection: ");
+            selection = Console.ReadLine();
+            string descision = "Very well. " + selection;
+
+            Console.WriteLine(descision);
         }
     }
 }
