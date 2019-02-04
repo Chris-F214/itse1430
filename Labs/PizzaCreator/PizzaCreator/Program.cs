12ec02303f24cbfe2978ba12414793dfcc8dcce2
Christopher Fields <christopher.fields214@my.tccd.edu>
2019-02-03 20:22:49 -0600

    Lab Work 2-3-19

--- Labs/PizzaCreator/PizzaCreator/Program.cs
+++ Labs/PizzaCreator/PizzaCreator/Program.cs
@@ -10,18 +10,78 @@ namespace PizzaCreator
     {
         static void Main( string[] args )
         {
-            string selection;
+            Showmenu();
+        }
+        private static void Showmenu()
+        {
             Console.WriteLine("Welcome to Papa Ceaser's Pizza");
             Console.WriteLine("What would you like to do?");
             Console.WriteLine("1.) Start new order");
             Console.WriteLine("2.) Modify order");
             Console.WriteLine("3.) Display order");
             Console.WriteLine("4.) Exit");
-            Console.WriteLine("Please make a selection: ");
-            selection = Console.ReadLine();
-            string descision = "Very well. " + selection;
+            Console.WriteLine("Please make a selection. ");
+            string MainMenuChoice = Console.ReadLine();
+            switch (MainMenuChoice)
+            {
+                case "1":
+                    {
+                            decimal NewPizzaSize = 0;
+                            Console.WriteLine("Please pick a size.");
+                            Console.WriteLine("1.) Small $5");
+                            Console.WriteLine("2.) Medium $6.25");
+                            Console.WriteLine("3.) Large $8.75");
+                            string SizeChoice = Console.ReadLine();
+                        switch (SizeChoice)
+                        {
+                            case "1":
+                                {
+                                    NewPizzaSize = 5;
+                                    var SizeSm = String.Format("Your Price is: {0:C}", NewPizzaSize);
+                                    Console.WriteLine(SizeSm);
+                                    break;
+                                }
 
-            Console.WriteLine(descision);
+                            case "2":
+                                {
+                                    NewPizzaSize = 6.25m;
+                                    var SizeM = String.Format("Your Price is: {0:C}", NewPizzaSize);
+                                    Console.WriteLine(SizeM);
+                                    break;
+                                }
+                            case "3":
+                                {
+                                    NewPizzaSize = 8.75m;
+                                    var SizeM = String.Format("Your Price is: {0:C}", NewPizzaSize);
+                                    Console.WriteLine(SizeM);
+                                    break;
+                                }
+                        }
+                        Console.WriteLine("Please select a meat.");
+                        Console.WriteLine("1.) Bacon");
+                        Console.WriteLine("2.) Ham");
+                        Console.WriteLine("3.) Pepperoni");
+                        Console.WriteLine("4.) Sausage");
+                        Console.WriteLine("5.) None");
+                        Console.ReadLine();
+                        break;
+                    }
+
+
+                case "2":
+                    {
+                            Console.WriteLine("incomplete");
+                            break;
+                    }
+                case "3":
+                    {
+                        break;
+                    }
+                case "4":
+                    {
+                        break;
+                    }
+            }
         }
     }
 }
