24fb6798375886365b8b3e3bc52da40f07e094a1
Christopher Fields <christopher.fields214@my.tccd.edu>
2019-02-07 12:48:39 -0600

    Comments added

--- Labs/PizzaCreator/PizzaCreator/Program.cs
+++ Labs/PizzaCreator/PizzaCreator/Program.cs
@@ -14,6 +14,7 @@ namespace PizzaCreator
         }
         private static void Showmenu()
         {
+            //Menu Options
             Console.WriteLine("Welcome to Papa Ceaser's Pizza");
             Console.WriteLine("What would you like to do?");
             Console.WriteLine("1.) Start new order");
@@ -26,7 +27,8 @@ namespace PizzaCreator
             {
                 case "1":
                     {
-                            decimal NewPizzaSize = 0;
+                            //Size Selection
+                            decimal PizzaCost = 0;
                             Console.WriteLine("Please pick a size.");
                             Console.WriteLine("1.) Small $5");
                             Console.WriteLine("2.) Medium $6.25");
@@ -36,34 +38,107 @@ namespace PizzaCreator
                         {
                             case "1":
                                 {
-                                    NewPizzaSize = 5;
-                                    var SizeSm = String.Format("Your Price is: {0:C}", NewPizzaSize);
+                                    PizzaCost = 5;
+                                    var SizeSm = String.Format("Your Price is: {0:C}", PizzaCost);
                                     Console.WriteLine(SizeSm);
                                     break;
                                 }
 
                             case "2":
                                 {
-                                    NewPizzaSize = 6.25m;
-                                    var SizeM = String.Format("Your Price is: {0:C}", NewPizzaSize);
+                                    PizzaCost = 6.25m;
+                                    var SizeM = String.Format("Your Price is: {0:C}", PizzaCost);
                                     Console.WriteLine(SizeM);
                                     break;
                                 }
                             case "3":
                                 {
-                                    NewPizzaSize = 8.75m;
-                                    var SizeM = String.Format("Your Price is: {0:C}", NewPizzaSize);
+                                    PizzaCost = 8.75m;
+                                    var SizeM = String.Format("Your Price is: {0:C}", PizzaCost);
                                     Console.WriteLine(SizeM);
                                     break;
                                 }
                         }
-                        Console.WriteLine("Please select a meat.");
-                        Console.WriteLine("1.) Bacon");
-                        Console.WriteLine("2.) Ham");
-                        Console.WriteLine("3.) Pepperoni");
-                        Console.WriteLine("4.) Sausage");
-                        Console.WriteLine("5.) None");
-                        Console.ReadLine();
+                        //Meat Selection
+                        bool MeatMenuFinished = false;
+                        Console.WriteLine("Please select a meat. Each selection is $0.75 extra.");
+                        Console.WriteLine("Your current price: {0:C}", PizzaCost);
+                        bool bacon = false;
+                        Console.WriteLine("1.) Bacon: " + bacon);
+                        bool ham = false;
+                        Console.WriteLine("2.) Ham: " + ham);
+                        bool pepperoni = false;
+                        Console.WriteLine("3.) Pepperoni: " + pepperoni);
+                        bool sausage = false;
+                        Console.WriteLine("4.) Sausage: " + sausage);
+                        Console.WriteLine("5.) Next");
+                        string MeatChoice = Console.ReadLine();
+                        switch (MeatChoice)
+                        {
+                            case "1":
+                            {
+                                    
+                                    if (bacon == false)
+                                        {
+                                        bacon = true;
+                                        PizzaCost += 0.75m;
+                                        }
+                                    else
+                                    {
+                                        bacon = false;
+                                        PizzaCost -= 0.75m;
+                                    }
+                                    break;
+                            }
+                            case "2":
+                            {
+                                    if (ham == false)
+                                    {
+                                        ham = true;
+                                        PizzaCost += 0.75m;
+                                    }
+                                    else
+                                    {
+                                        ham = false;
+                                        PizzaCost += 0.75m;
+                                    }
+                                    return MeatMenu;
+                                }
+                            
+                            case "3":
+                            {
+                                    if (pepperoni == false)
+                                    {
+                                        pepperoni = true;
+                                        PizzaCost += 0.75m;
+                                    } else
+                                    {
+                                        pepperoni = false;
+                                        PizzaCost += 0.75m;
+                                    }
+                                    return MeatMenu;
+                                }
+                            case "4":
+                            {
+                                    if (sausage == false)
+                                    {
+                                        sausage = true;
+                                        PizzaCost += 0.75m;
+                                    }
+                                    else
+                                    {
+                                        sausage = false;
+                                        PizzaCost -= 0.75m;
+                                    }
+                                    return MeatMenu;
+                                }
+                            case "5":
+                            {
+                                    Console.WriteLine("Please select a vegetable.");
+                                    Console.WriteLine("Your current price is: {0:C}", PizzaCost);
+                            }
+                                break;
+                        }
                         break;
                     }
 
